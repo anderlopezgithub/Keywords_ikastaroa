1cf73257a1a075b7fc67bd26d80601045c848438
anderlopezgithub <anderlopez@gmail.com>
2024-05-09 10:57:09 +0000

    1. Ariketa

--- 1 keywords(Hasiera)/input_data.py
+++ 1 keywords(Hasiera)/input_data.py
@@ -21,6 +21,9 @@ keywords = {
     "secretaria": "request_contacto_horarios",
     "horarios": "request_contacto_horarios",
 
+    "inscripcion": "request_inscripcion",
+    "inscribirme": "request_inscripcion",
+    "inscribir": "request_inscripcion",
 
     "prematricula" : "request_pre_matricula",
     "pre-matricula": "request_pre_matricula",
@@ -32,9 +35,7 @@ keywords = {
     "pre-matricularme": "request_pre_matricula",
     "pre-matricular": "request_pre_matricula",
     "pre-matriculacion": "request_pre_matricula",
-    "inscripcion": "request_pre_matricula",
-    "inscribirme": "request_pre_matricula",
-    "inscribir": "request_pre_matricula",
+    
     "fechas": "request_pre_matricula",
     "matriculas": "request_pre_matricula",
     "matricula" : "request_matricula",
@@ -44,23 +45,25 @@ keywords = {
    
 
     "contacto" : "request_contacto",
-    "telefono": "request_contacto",
-    "tlfno": "request_contacto",
-    "email": "request_contacto",
     "direccion": "request_contacto",
     "localizacion": "request_contacto",
     "sitio": "request_contacto",
     "encuentra": "request_contacto",
-    "e-mail": "request_contacto",
-    "tlfn": "request_contacto",
-    "llamar": "request_contacto",
-    "llamada": "request_contacto",
-    "correo": "request_contacto",
-    "electronico": "request_contacto",
-    "gmail": "request_contacto",
-    "hotmail": "request_contacto",
-    "g-mail": "request_contacto",
-    "mail": "request_contacto",
+
+    "telefono": "request_telefono",
+    "tlfno": "request_telefono",
+    "tlfn": "request_telefono",
+    "llamar": "request_telefono",
+    "llamada": "request_telefono",
+
+    "email": "request_email",
+    "e-mail": "request_email",
+    "correo": "request_email",
+    "electronico": "request_email",
+    "gmail": "request_email",
+    "hotmail": "request_email",
+    "g-mail": "request_email",
+    "mail": "request_email",
       
 
     "oferta": "request_oferta",
@@ -85,9 +88,12 @@ answer = {
     "request_saludo": "¡Buenos días! Puedo ayudarte a contactar con el centro, proporcionarte información sobre prematricula/matricula y oferta", 
     "request_despedida": "Espero haberte ayudado. Gracias por tu visita! Hasta pronto!",
     "request_contacto_horarios" : "El horario del centro es de 08:00-17:30. El horario de secretaría es de 09:00-14:30. ",
+    "request_inscripcion": "¿Sobre qué quieres más información de las inscripciones, de la pre-matrículo o de la matrícula?",
     "request_pre_matricula": "El periodo de pre-matrícula es a partir de junio. Si rellenas el siguiente formulario (xxx (URL), cuando se publiquen las fechas oficiales te podremos avisar. Puedes inscribirte aquí: xxx (URL)",
     "request_matricula": "Para realizar la matrícula debes esperar a julio. La matrícula puedes realizarla aquí: xxx (URL)", 
-    "request_contacto" : "Los datos de contacto del centro son: Teléfono: XXX XXX XXX. Email: xxxxxxx@gmail.com ",
+    "request_contacto" : "Los datos de contacto del centro son: Teléfono: 945 233 714. Email: icjardin@icjardin.net ",
+    "request_telefono" : "El teléfono de contacto del centro es: Teléfono: 945 233 714.",
+    "request_email" : "El email de contacto del centro es: icjardin@icjardin.net.",
     "request_oferta": "El centro tiene una gran oferta de formaciones para realizar. Puedes informarte sobre ellas aquí: xxx (URL)",
 
     "request_unknown": "¡Vaya! Parece que no te he entendido muy bien. Puedo ayudarte a contactar con el centro, proporcionarte información sobre matricula y oferta"
